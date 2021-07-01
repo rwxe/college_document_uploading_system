ca0ac5ed7b73d820c72cef8ab67e298e11b11197
yizdu <jzunk@protonmail.com>
2021-07-01 19:33:17 +0800

    *

--- college_document_uploading_system/cdus/views.py
+++ college_document_uploading_system/cdus/views.py
@@ -34,8 +34,6 @@ def index(request):
 def qft(request):
     return HttpResponse("快速功能测试")
 
-def register(request):
-    pass
 
 def login(request):
     if request.method == 'GET':
@@ -48,7 +46,7 @@ def login(request):
         try:
             result = models.Teacher.objects.get(email=email)
             if hashers.check_password(p1, result.password):
-                request.session['username'] = result.username
+                request.session['name'] = result.name
                 request.session['id'] = result.id
                 # 登录成功
                # context = {'the_url': reverse('cdus:index'),
@@ -69,7 +67,113 @@ def login(request):
             return render(request, 'cdus/login.html', context)
         except:
             ex_type, ex_val, ex_stack = sys.exc_info()
+            print(sys.exc_info())
             return HttpResponse("出BUG了")
     else:
         return HttpResponse("出BUG了")
-    pass
+
+
+def register(request):
+    # 注册页面
+    if request.method == 'GET':
+        # 是否发送了验证码
+        request.session['sent'] = 'no'
+        context = {'err_msg': '',
+                   'sent': 'no',
+                   }
+        return render(request, 'cdus/register.html', context)
+    elif request.method == 'POST':
+        # print("会话项目",request.session.items())
+        # print("会话过期秒数",request.session.get_expiry_age())
+        # print("会话到期时间",request.session.get_expiry_date())
+        name = request.POST.get('name')
+        email = request.POST.get('email')
+        p1 = request.POST.get('password1')
+        p2 = request.POST.get('password2')
+        if request.session.get('sent') == 'no':
+            # 这里由于偷懒，并没有记录是否发送成功了
+            try:
+                send_verify_code(request, email)
+            except Exception as e:
+                print(e)
+                print(sys.exc_info())
+                request.session['sent'] = 'no'
+                context = {'err_msg': '验证码的发送出现了问题，这可能在短时间内无法解决，请改日再来',
+                           'sent': 'no',
+                           }
+                return render(request, 'cdus/register.html', context)
+
+            context = {'name': name,
+                       'email': email,
+                       'password1': p1,
+                       'password2': p2,
+                       'sent': 'yes'
+                       }
+            request.session['sent'] = 'yes'
+            return render(request, 'cdus/register.html', context)
+        if request.session.get('sent') == 'yes':
+            if request.session.get('verify_code') != request.POST.get('verify_code'):
+                context = {'err_msg': '验证码输入错误',
+                           'name': name,
+                           'email': email,
+                           'password1': p1,
+                           'password2': p2,
+                           'sent': 'yes',
+                           }
+                return render(request, 'cdus/register.html', context)
+        if request.session.get('sent') == None:
+            context = {'err_msg': '验证码过期，请重新获取',
+                       'sent': 'no',
+                       }
+            return render(request, 'cdus/register.html', context)
+
+        new_user = models.User()
+        name = request.POST.get('name')
+        # 邮箱也要检测是否唯一
+        email = request.POST.get('email')
+        p1 = request.POST.get('password1')
+        p2 = request.POST.get('password2')
+
+        try:
+            # 检测数据库中是否已存在同名用户，若抛出该用户
+            # 名不存在异常，则为可以存入数据库
+            models.User.objects.get(name=name)
+            context = {'err_msg': '此用户名已存在，请更换一个',
+                       }
+            return render(request, 'cdus/register.html', context)
+        except models.User.DoesNotExist:
+            new_user.name = name
+
+        try:
+            # 检测数据库中是否已存在相同邮箱，若抛出该邮箱
+            # 不存在异常，则为可以存入数据库
+            models.User.objects.get(email=email)
+            context = {'err_msg': '此邮箱已存在，请更换一个',
+                       }
+            return render(request, 'cdus/register.html', context)
+        except models.User.DoesNotExist:
+            new_user.email = email
+
+        if p1 != p2:
+            context = {'err_msg': '两次输入的密码不同，请检查是否有误',
+                       }
+            return render(request, 'cdus/register.html', context)
+        else:
+            p1_encrypted = hashers.make_password(p1, None, 'pbkdf2_sha256')
+            new_user.password = p1_encrypted
+            try:
+                new_user.save()
+                # 注册成功
+               # context = {'the_url': reverse('cdus:index'),
+               #           'hint': '注册成功了',
+               #           'page': '主页',
+               #           }
+               # return render(request, 'cdus/hint.html', context)
+                return hint_and_redirect(request, reverse('cdus:index'), '注册成功了')
+
+            except DataError:
+                context = {'err_msg': '数据错误，请检查您输入的内容是否符合格式',
+                           }
+                return render(request, 'cdus/register.html', context)
+            except:
+                return HttpResponse("出BUG了")
